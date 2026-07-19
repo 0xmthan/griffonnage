75f25826a42237e18290b125346cfbcf2e1d290c
mthan <me@mthan.dev>
2026-07-19 18:14:48 +0300

    malloc @ free time 2025-11-19 w/ atursun
    Co-authored-by: abdullah tursun <atursun@student.42istanbul.com.tr>

---
+++ malloc_train.c
@@ -0,0 +1,27 @@
+// 19 Nov 2025 15:10
+// 42 Piscine free time 🔔
+// tutor atursun
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+	// statik bellek tahsisi
+	int arr2[5] = {1, 2, 3, 4, 5};
+
+	// dinamik bellek tahsisi
+	int *arr = malloc(5);
+	arr[0] = 1;
+	arr[1] = 2;
+	arr[2] = 3;
+	arr[3] = 4;
+	arr[4] = 5;
+
+	free(arr);
+
+	
+	return 0;
+}
